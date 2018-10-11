4915249fc500d55079855cd18aeb266c486d151b
hossain24 <haj.ctg@gmail.com>
2018-10-11 22:19:45 +0200

    front-end added

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 mongoose.connect("mongodb://localhost/hilltrack");
 mongoose.Promise = global.Promise;
 
+app.use(express.static("public"));
+
 app.use(bodyParser.json());
 
 // initilize routes
